546cabe9a198bf930e1ee2c89e3192b4aed80d0c
spr26 <96800585+spr26@users.noreply.github.com>
2025-07-23 21:17:58 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://upjkflvabqwunqujhhhy.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey)
 
 export type Database = {
   public: {
